07b0a47ff6ca6139f881673a6588e0cd50c71f15
xania <34893682+xania@users.noreply.github.com>
2022-01-22 23:14:27 +0100

    capture image

---
+++ src/camera/index.tsx
@@ -0,0 +1,53 @@
+import { jsx } from "@xania/view";
+import { createHtmlElement } from "@xania/view/lib/util/create-dom";
+
+import "./style.scss";
+
+// Set constraints for the video stream
+const constraints = { video: { facingMode: "user" }, audio: false };
+
+export function CameraComponent() {
+  const canvasElt = createHtmlElement("canvas");
+
+  const videoElt = createHtmlElement("video");
+  videoElt.setAttribute("autoplay", "true");
+  videoElt.setAttribute("playsinline", "true");
+
+  const imgElt = createHtmlElement("img");
+  imgElt.src = "//:0";
+  imgElt.alt = "";
+
+  //  <img src="//:0" alt="" id="camera--output">
+
+  return (
+    <div>
+      {videoElt}
+      {canvasElt}
+      {imgElt}
+      <button click={(e) => capture()}>capture</button>
+      {startCamera}
+    </div>
+  );
+
+  function capture() {
+    canvasElt.width = videoElt.videoWidth;
+    canvasElt.height = videoElt.videoHeight;
+    canvasElt.getContext("2d").drawImage(videoElt, 0, 0);
+    imgElt.src = canvasElt.toDataURL("image/webp");
+    imgElt.classList.add("taken");
+  }
+
+  function startCamera() {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((stream) => {
+        const track = stream.getTracks()[0];
+        console.log(track);
+        videoElt.srcObject = stream;
+        stream;
+      })
+      .catch((err) => {
+        console.error("Oops. Something is broken.", err);
+      });
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
+    https: true,
     proxy: {
       "/api": {
         target: "http://localhost:7071",
