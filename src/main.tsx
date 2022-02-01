2c05aa7a609c6648c344e7ccd3c2fad40079ce25
xania <34893682+xania@users.noreply.github.com>
2022-02-01 03:04:04 +0100

    component routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,26 @@ import "./style.css";
 import { jsx, render } from "@xania/view";
 // import CreatePortlet from "./portlets/create-portlet";
 import { CameraComponent } from "./jennah/camera";
-import { BrowserOutlet, anyRoute } from "@xania/router";
+import { BrowserOutlet, fallback } from "@xania/router";
 import { route } from "@xania/router";
-import { ListDirectories } from "./services/storage";
 import { Receipts } from "./jennah/receipts";
+import { RouteContext } from "@xania/router";
+
+class Test {
+  constructor(private ctx: RouteContext) {}
+  get view(): any {
+    return <div>[test de test] {this.ctx.path.join("-")}</div>;
+  }
+}
 
 render(
   <BrowserOutlet
+    render={render}
     routes={[
-      route("test", <Test />),
-      route("camera", <CameraComponent />),
-      route("receipts", <Receipts />),
-      anyRoute((context) => (
+      route("test/asdafs/asfdasd", (ctx) => new Test(ctx)),
+      route("camera", CameraComponent),
+      route("receipts", Receipts),
+      fallback((context) => (
         <div>
           <div style="color: gray; font-size: 40px;">404</div>
           <div>/{context.url.path.join("/")}</div>
@@ -23,10 +31,6 @@ render(
   />,
   "#app"
 );
-
-function Test() {
-  return <ListDirectories></ListDirectories>;
-}
 
 // render(<CameraComponent />, "#app", { name: "Ibrahim" });
 
--- src/services/storage/index.tsx
+++ src/services/storage/index.tsx
@@ -1,35 +1,35 @@
-import { ContainerClient } from "@azure/storage-blob";
-import { createContainer, useContext, ViewContainer } from "@xania/view";
-import { jsx } from "@xania/view";
+// import { ContainerClient } from "@azure/storage-blob";
+// import { createContainer, useContext, ViewContainer } from "@xania/view";
+// import { jsx } from "@xania/view";
 
-export function ListDirectories() {
-  var client = new ContainerClient(
-    "https://appcomponents.blob.core.windows.net/xania"
-  );
+// export function ListDirectories() {
+//   var client = new ContainerClient(
+//     "https://appcomponents.blob.core.windows.net/xania"
+//   );
 
-  var container = createContainer();
-  var result = client.listBlobsByHierarchy("/", {});
-  iterate(container, result);
-  const $ = useContext<any>();
-  return (
-    <div>
-      {container.map(
-        <div>
-          <span>-/</span>
-          {$("name")}
-        </div>
-      )}
-    </div>
-  );
-}
+//   var container = createContainer();
+//   var result = client.listBlobsByHierarchy("/", {});
+//   iterate(container, result);
+//   const $ = useContext<any>();
+//   return (
+//     <div>
+//       {container.map(
+//         <div>
+//           <span>-/</span>
+//           {$("name")}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
-function iterate(c: ViewContainer, asyncIterator: { next(): any }) {
-  asyncIterator.next().then(({ value, done }) => {
-    if (value?.kind === "prefix") {
-      c.push([value]);
-    }
-    if (!done) {
-      iterate(c, asyncIterator);
-    }
-  });
-}
+// function iterate(c: ViewContainer, asyncIterator: { next(): any }) {
+//   asyncIterator.next().then(({ value, done }) => {
+//     if (value?.kind === "prefix") {
+//       c.push([value]);
+//     }
+//     if (!done) {
+//       iterate(c, asyncIterator);
+//     }
+//   });
+// }
