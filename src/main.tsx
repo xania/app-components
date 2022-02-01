75422e085dd2c25c346fe2901242be57f9a47fe7
xania <34893682+xania@users.noreply.github.com>
2022-02-01 03:47:46 +0100

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import "./style.css";
 import { jsx, render } from "@xania/view";
 // import CreatePortlet from "./portlets/create-portlet";
 import { CameraComponent } from "./jennah/camera";
-import { BrowserOutlet, fallback } from "@xania/router";
+import { WebApp, fallback } from "@xania/router";
 import { route } from "@xania/router";
 import { Receipts } from "./jennah/receipts";
 import { RouteContext } from "@xania/router";
@@ -14,36 +14,17 @@ class Test {
   }
 }
 
-render(
-  <BrowserOutlet
-    render={render}
-    routes={[
-      route("test/asdafs/asfdasd", (ctx) => new Test(ctx)),
-      route("camera", CameraComponent),
-      route("receipts", Receipts),
-      fallback((context) => (
-        <div>
-          <div style="color: gray; font-size: 40px;">404</div>
-          <div>/{context.url.path.join("/")}</div>
-        </div>
-      )),
-    ]}
-  />,
-  "#app"
-);
-
-// render(<CameraComponent />, "#app", { name: "Ibrahim" });
-
-// function App() {
-//   return (
-//     <section>
-//       <header class="top-bar">App Workspace</header>
-//       <section class="jumbotron">
-//         Select components for the your App with no code, we take care for
-//         everything else.
-//       </section>
-//       <CreatePortlet title="Hello Portlet" />
-//       <CameraComponent />
-//     </section>
-//   );
-// }
+<WebApp
+  render={render}
+  routes={[
+    route("test/asdafs/asfdasd", (ctx) => new Test(ctx)),
+    route("camera", CameraComponent),
+    route("receipts", Receipts),
+    fallback((context) => (
+      <div>
+        <div style="color: gray; font-size: 40px;">404</div>
+        <div>/{context.url.path.join("/")}</div>
+      </div>
+    )),
+  ]}
+/>;
