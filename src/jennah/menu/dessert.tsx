81e275aad3708dbb9f4f1aa503b2b4dbc2e9c78e
xania <34893682+xania@users.noreply.github.com>
2022-05-28 10:22:16 +0200

    hack cont

--- src/jennah/menu/dessert.tsx
+++ src/jennah/menu/dessert.tsx
@@ -1,36 +1,36 @@
 import { jsx } from "@xania/view";
 import { TopBar } from "./menu-card";
-// import styles from "./desserts/style.module.scss";
-// import chizkoffie from "./desserts/cheesecake-koffie.jpg";
-// import chizfruit from "./desserts/cheesecake-fruit.jpg";
-// import lavaice from "./desserts/lava-ice.jpg";
-// import redvelvet from "./desserts/redvelvet.jpg";
-// import carrot from "./desserts/carrot.jpg";
-// import icecup from "./desserts/ice.jpg";
+import styles from "./desserts/style.module.scss";
+import chizkoffie from "./desserts/cheesecake-koffie.jpg";
+import chizfruit from "./desserts/cheesecake-fruit.jpg";
+import lavaice from "./desserts/lava-ice.jpg";
+import redvelvet from "./desserts/redvelvet.jpg";
+import carrot from "./desserts/carrot.jpg";
+import icecup from "./desserts/ice.jpg";
 
 export function DessertMenu() {
   return (
     <div>
       <TopBar subtitle="Desserts" />
       <div style="display: flex; padding: 40px;">
-        {/* <Dessert imageUrl={lavaice} />
+        <Dessert imageUrl={lavaice} />
         <Dessert imageUrl={chizkoffie} />
         <Dessert imageUrl={chizfruit} />
         <Dessert imageUrl={redvelvet} />
         <Dessert imageUrl={carrot} />
-        <Dessert imageUrl={icecup} /> */}
+        <Dessert imageUrl={icecup} />
       </div>
     </div>
   );
 }
 
-// interface DessertProps {
-//   imageUrl: string;
-// }
-// function Dessert(props: DessertProps) {
-//   return (
-//     <div class={styles["dessert"]}>
-//       <img src={props.imageUrl} />
-//     </div>
-//   );
-// }
+interface DessertProps {
+  imageUrl: string;
+}
+function Dessert(props: DessertProps) {
+  return (
+    <div class={styles["dessert"]}>
+      <img src={props.imageUrl} />
+    </div>
+  );
+}
